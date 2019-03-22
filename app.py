77f1f917998a87bb8154e17b086d14e26664cd3c
BhargavKilambi <bhargavramkilambi@gmail.com>
2019-03-23 01:15:02 +0530

    Some updates after pyrebase

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask,render_template
+"""
+Basic Flask app integrated with Firebase using Pyrebase
+"""
+#imports
+from flask import Flask,render_template,request,redirect
 import pyrebase
 
+#saving firebase config in a class
+from config1 import Config1
+#init flask
 app = Flask(__name__)
+user = None
 user_1 = False
-config = {
-    "apiKey": "AIzaSyDNfqGKMpgclIHcpl4-kbTwRAtTHBkH1pU",
-    "authDomain": "pytrial-cd85e.firebaseapp.com",
-    "databaseURL": "https://pytrial-cd85e.firebaseio.com",
-    "projectId": "pytrial-cd85e",
-    "storageBucket": "pytrial-cd85e.appspot.com",
-    "messagingSenderId": "669169177880"
-}
-firebase  = pyrebase.initialize_app(config)
+
+#config firebase
+c1 = Config1()
+firebase  = pyrebase.initialize_app(c1.giveConfig())
+auth = firebase.auth()
+database = firebase.database()
+
+##SIMPLE ROUTES##
+
+#home route
 @app.route('/')
 def home():
     if user_1:
@@ -19,5 +28,44 @@ def home():
     else:
         return render_template('login.html')
 
+#login to firebase
+@app.route('/signin',methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        formdata = request.form
+        try:
+            user = auth.sign_in_with_email_and_password(formdata['email'],formdata['password'])
+        except:
+            if len(formdata['password'])<8:
+                error = "* Password must be atleast 8 characters"
+            else:
+                error = "* Invalid email/password"
+            return render_template('login.html',error = error)
+        finally:
+            user_1 = True
+            user = user
+            return render_template('home.html',u = user)
+        if user:
+            user_1 = True
+            user = user
+            return render_template('home.html',u = user)
+        else:
+            error = "* Invalid email/password"
+            return render_template('login.html',error = error)
+    else:
+        return redirect('/signin')
+
+#signout
+@app.route('/signout')
+def logout():
+    if auth.current_user:
+        user_1 = False
+        user = None
+        return redirect('/')
+    else:
+        user_1 = False
+        return redirect('/')
+
+#app main route
 if __name__ == "__main__":
     app.run(debug = True)
---
+++ config1.py
@@ -0,0 +1,12 @@
+config = {
+    "apiKey": "AIzaSyDNfqGKMpgclIHcpl4-kbTwRAtTHBkH1pU",
+    "authDomain": "pytrial-cd85e.firebaseapp.com",
+    "databaseURL": "https://pytrial-cd85e.firebaseio.com",
+    "projectId": "pytrial-cd85e",
+    "storageBucket": "pytrial-cd85e.appspot.com",
+    "messagingSenderId": "669169177880"
+}
+
+class Config1:
+    def giveConfig(self):
+        return config
